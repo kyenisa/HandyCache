8353cf9c1032c3e357953d502994f818f374f9c0
Luk <truefriend.cz@gmail.com>
2017-08-25 13:53:04 +0200

    Delete Modules.lua

--- Selector/Modules.lua
+++
@@ -1,6 +0,0 @@
---[[ <HCExtension>
-@name			------------------- Modules - (section down) --------------------------------------
-@author			truefriend-cz
-@version		1.1
-@license		CC BY-NC-SA (Attribution-NonCommercial-ShareAlike - https://creativecommons.org/licenses/by-nc-sa/) 
-</HCExtension> ]]
